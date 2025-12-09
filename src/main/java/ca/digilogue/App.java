312e0636f4d5ad47f2e1506a8a690ff5bc88e193
Gary Black <gar2000b@yahoo.com>
2025-12-08 19:05:13 -0500

    Initial push

---
+++ src/main/java/ca/digilogue/App.java
@@ -0,0 +1,13 @@
+package ca.digilogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        System.out.println("xp-users-service is starting up...");
+    }
+}
