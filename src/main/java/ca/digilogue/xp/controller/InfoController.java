4396151a21d164d743a2ccdd4f9c04a150acb430
Gary Black <gar2000b@yahoo.com>
2025-12-09 15:28:13 -0500

    Updating endpoints with logging.

--- src/main/java/ca/digilogue/xp/controller/InfoController.java
+++ src/main/java/ca/digilogue/xp/controller/InfoController.java
@@ -3,21 +3,38 @@ package ca.digilogue.xp.controller;
 import ca.digilogue.xp.App;
 import ca.digilogue.xp.model.Info;
 import ca.digilogue.xp.model.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
 
+    private static final Logger log = LoggerFactory.getLogger(InfoController.class);
+
     @GetMapping("/test")
-    public Test getTestMessage() {
-        return new Test("XP Users Service is alive!");
+    public ResponseEntity<Test> getTestMessage() {
+        Test response = new Test("XP Users Service is alive!");
+
+        // Log JSON-style representation
+        log.info("GET /test JSON Response: {{\"message\":\"{}\"}}", response.getMessage());
+
+        return ResponseEntity.ok(response); // 200 OK
     }
 
     @GetMapping("/info")
-    public Info getServiceInfo() {
-        return new Info("XP Users Service", App.version);
+    public ResponseEntity<Info> getServiceInfo() {
+        Info response = new Info("XP Users Service", App.version);
+
+        // Log JSON-style representation
+        log.info(
+                "GET /info JSON Response: {{\"appName\":\"{}\",\"version\":\"{}\"}}",
+                response.getAppName(),
+                response.getVersion()
+        );
+
+        return ResponseEntity.ok(response); // 200 OK
     }
-
-
 }
